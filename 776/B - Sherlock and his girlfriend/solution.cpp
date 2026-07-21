fc7acb02481800b72bfe529b7a5515db85ebaa0d
Sweta Jha <swetajhaofficial11@gmail.com>
2026-07-21 15:29:12 +0530

    Add Sherlock and his girlfriend [B] from Codeforces

---
+++ 776/B - Sherlock and his girlfriend/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+   int n;
+   cin>> n;
+   int price[n+2];
+   for(int i=1;i<=n+1;i++){
+       price[i]=i+1;
+   }
+   vector<bool> prime(n+2,true);
+   prime[0]=prime[1]=false;
+   for(int i=2;i*i<=n+1;i++){
+       if(prime[i]){
+         for(int j=i*i;j<=n+1;j+=i){
+            prime[j]= false;
+         }
+       }
+   }
+   vector<int> color(n+2,1);
+   for(int i=2;i<=n+1;i++){
+       if(!prime[i]) color[i]=2;
+   }
+       int total= *max_element(color.begin(),color.end());
+       cout<< total<< endl;
+       for(int i=2;i<=n+1;i++){
+           cout<< color[i]<< " ";
+       }
+    return 0;
+}
